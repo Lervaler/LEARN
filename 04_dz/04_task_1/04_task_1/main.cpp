de2e2ef8a2844739697c2a69d7e833c8a42ffea5
Valeria <ler.valeu@gmail.com>
2022-02-12 20:06:39 +0300

    определение мин и мах

---
+++ 04_dz/04_task_1/04_task_1/main.cpp
@@ -0,0 +1,46 @@
+// Поменять местами максимальный и последний отрицательный элементы массива M(40).
+
+#include <time.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+int main()
+{
+    constexpr int size = 40;
+    int M [size];
+    int updated_M [size];
+    int index_updated_M = 0;
+
+    srand(10% - 100);        // почему тут Srand, а дальше по тексту rand . Это одно и тоже??
+
+    for ( int i = 0; i < size; ++i)
+    {
+        M [i] = rand ()% 100 - 50;
+        printf ("%d ", M[i]);
+    }
+    printf ("\n");
+
+    int max = 0;             //максимальное число
+    int last_min = 0;        // последнее отрицательное
+
+    for (int i = 0; i<100; i++)
+     {
+        if (M[i] > max && M[i] <100 && M[i]>0)
+        {
+         max = M[i];
+        }
+      }
+      printf ("max = %d\n", max);
+
+      for (int i = 0; i < 100; i++)
+      {
+          if (M[i] < last_min && M[i]<0 && M[i]>-100)   //пока не последний минимум, а просто самый минимум
+          {
+           last_min = M[i];
+          }
+       }
+       printf ("last_min = %d\n", last_min);
+
+
+    return 0;
+}
