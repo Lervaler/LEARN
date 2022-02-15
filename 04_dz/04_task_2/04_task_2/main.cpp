9505ef2f79b9227b09fe40ef54e7a70ccaaa0013
Valeria <ler.valeu@gmail.com>
2022-02-15 21:29:11 +0300

    найдены значения, надо найти место

---
+++ 04_dz/04_task_2/04_task_2/main.cpp
@@ -0,0 +1,61 @@
+//Определить местоположение элементов массива А (30), не встречающихся в массиве В (15).
+
+#include <time.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+int main()
+{
+    constexpr int sizeA = 30;
+    int A [sizeA];
+    constexpr int sizeB = 15;
+    int B [sizeB];
+    int E [sizeA];
+    int index_E = 0;
+
+    srand(100);
+
+    printf ("MASSIVE A: ");
+    for (int i = 0; i < sizeA; ++i)
+    {
+        A [i] = rand ()% 20;                 //делаем рандом
+        printf ("%d ", A [i]);
+    }
+    printf ("\n");
+
+    printf ("MASSIVE B: ");
+    for (int i = 0; i < sizeB; ++i)
+    {
+        B [i] = rand ()% 20;                 //делаем рандом
+        printf ("%d ", B [i]);
+    }
+    printf ("\n");
+
+    printf ("non-B elements of A is targets: \n");    // местоположение в массиве А
+    printf ("           ");                           // это, чтобы строчки были вровень
+
+
+    for (int i = 0; i < sizeA; ++i)
+    {
+     bool found = false;
+             for (int j = 0; j < sizeB && !found; ++j)
+             {
+                 if (A[i] == B[j])
+                 found = true;
+             }
+         if (!found)
+         {
+          E [index_E] = A[i];
+          ++index_E;
+          }
+       }
+             for(int i = 0; i < index_E; ++i)
+              {
+                  printf("%d ", E [i]);
+              }
+
+
+    printf ("\n");
+
+   return 0;
+}
