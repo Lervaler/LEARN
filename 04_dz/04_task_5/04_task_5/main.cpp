ca4fff1c0c69d47d90016c0dd160625c4c483e6a
Valeria <ler.valeu@gmail.com>
2022-02-22 18:15:24 +0300

    4

--- 04_dz/04_task_5/04_task_5/main.cpp
+++ 04_dz/04_task_5/04_task_5/main.cpp
@@ -11,26 +11,21 @@ int main()
 
     gets (str);
     char* word;
+    printf ("deleted ne-chetnye \n");
 
     char sep [10] = " ";
-       word = strtok(str, sep);
-       while (word != nullptr)
-        {
-           printf ("word is %s \n", word);
-           word = strtok (nullptr, sep);
+    word = strtok(str, sep);
 
-
-        if (word != nullptr && (strlen(word))%2 == 0)
-        {
-           strcpy (result_str, word);
-           printf ("chetnye - %s \n", result_str);
-         }
-}
-
-
-//printf ("chetnye - %s", result_str);
-
-    // puts (result_str);
+    while (word != nullptr)
+    {
+     //printf ("word is %s \n", word);
+        word = strtok (nullptr, sep);
+            if (word != nullptr && (strlen(word))%2 == 0)
+            {
+                 strcpy (result_str, word);
+                 printf ("%s ", result_str);
+            }
+    }
 
     return 0;
 }
