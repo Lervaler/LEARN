4a52661d65b72ab6ed8eebb06eb964a8844b5f5a
Valeria <ler.valeu@gmail.com>
2022-03-09 21:33:26 +0300

    пробелы

--- 04_dz/04_task_2/04_task_2/main.cpp
+++ 04_dz/04_task_2/04_task_2/main.cpp
@@ -1,72 +1,69 @@
 //Определить местоположение элементов массива А (30), не встречающихся в массиве В (15).
 
-#include <time.h>
+//#include <time.h> - использовать при рандоме от времени
 #include <stdlib.h>
 #include <stdio.h>
 
 int main()
 {
     constexpr int sizeA = 30;
-    int A [sizeA];
+    int A[sizeA];
     constexpr int sizeB = 15;
-    int B [sizeB];
-    int E [sizeA];
+    int B[sizeB];
+    int E[sizeA];
     int index_E = 0;
-
     srand(100);
 
-    printf ("MASSIVE A: ");
-    for (int i = 0; i < sizeA; ++i)
+    printf("MASSIVE A: ");
+    for(int i = 0; i < sizeA; ++i)
     {
-        A [i] = rand ()% 20;                 //делаем рандом
-        printf ("%d ", A [i]);
+        A[i] = rand()% 20;    //делаем рандом
+        printf("%d ", A[i]);
     }
-    printf ("\n");
+        printf("\n");
 
-    printf ("MASSIVE B: ");
-    for (int i = 0; i < sizeB; ++i)
+    printf("MASSIVE B: ");
+    for(int i = 0; i < sizeB; ++i)
     {
-        B [i] = rand ()% 20;                 //делаем рандом
-        printf ("%d ", B [i]);
+        B[i] = rand()% 20;    //делаем рандом
+        printf("%d ", B[i]);
     }
-    printf ("\n");
+        printf("\n");
 
-    printf ("non-B numbers: \n");    //  значение в массиве А
-    printf ("           ");                           // это, чтобы строчки были вровень
+    printf("non-B numbers: \n");    // несовпадающие значения в массиве А
+    printf("           ");    // это, чтобы строчки были вровень
 
-    for (int i = 0; i < sizeA; ++i)
+    for(int i = 0; i < sizeA; ++i)
     {
-    bool found = false;
-        for (int j = 0; j < sizeB && !found; ++j)
-        {
-            if (A[i] == B[j])
-            found = true;
-        }
-            if (!found)
+        bool found = false;
+            for(int j = 0; j < sizeB && !found; ++j)
             {
-                E [index_E] = A[i];
-                ++index_E;
+                if(A[i] == B[j])
+                    found = true;
             }
+                if(!found)
+                {
+                    E[index_E] = A[i];
+                    ++index_E;
+                }
     }
 
     for(int i = 0; i < index_E; ++i)
     {
-        printf("%d ", E [i]);
+        printf("%d ", E[i]);
     }
 
-    printf ("\n");
-    printf ("non-B location: \n");                     // местоположение в массиве А
-    printf ("           ");                           // это, чтобы строчки были вровень
+    printf("\n");
+    printf("non-B location: \n");    // местоположение в массиве А
+    printf("           ");    // это, чтобы строчки были вровень
 
     for(int i = 0; i < sizeA; ++i)
     {
-        for (int j = 0; j < index_E; ++j)
-        if (A[i]==E[j])
-        printf("%d ", ++i);
+        for(int j = 0; j < index_E; ++j)
+            if(A[i]==E[j])
+                printf("%d ", ++i);
     }
 
-    printf ("\n");
-    printf ("\n");
-
-   return 0;
+    printf("\n");
+    return 0;
 }
