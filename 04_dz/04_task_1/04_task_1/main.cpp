e248331b07034240f06ed32a35c903d2b28ffd4c
Valeria <ler.valeu@gmail.com>
2022-03-09 21:37:53 +0300

    пробелы

--- 04_dz/04_task_1/04_task_1/main.cpp
+++ 04_dz/04_task_1/04_task_1/main.cpp
@@ -7,51 +7,51 @@
 int main()
 {
     constexpr int size = 40;
-    int M [size];
-    int updated_M [size];
+    int M[size];
+    int updated_M[size];
     int index_updated_M = 0;
     int max = 0;    //максимальное число
     int last_min = 0;    //последнее отрицательное
-    srand(1000);    //надо разобраться с srand и rand  (при отключении просто меняется рандом)
+    srand(1000);    //надо разобраться с srand и rand (при отключении просто меняется рандом)
 
-    for (int i = 0; i < size; ++i)
+    for(int i = 0; i < size; ++i)
     {
-        M [i] = rand ()% 1000 - 500;    //делаем рандом
-        printf ("%d ", M[i]);
+        M[i] = rand()% 1000 - 500;    //делаем рандом
+        printf("%d ", M[i]);
     }
-        printf ("\n");
+        printf("\n");
 
-    for (int i = 0; i<size; ++i)    // считаем максимум
+    for(int i = 0; i<size; ++i)    // считаем максимум
     {
-        if (M[i] > max && M[i] < 1000 && M[i] > 0)    // тут меньше 1000 потому что я знаю диапазон, как сделать для любого диапазона??
+        if(M[i] > max && M[i] < 1000 && M[i] > 0)    //тут меньше 1000 потому что я знаю диапазон, как сделать для любого диапазона??
         {
             max = M[i];
         }
     }
-        printf ("max = %d\n", max);
+        printf("max = %d\n", max);
 
-    for (int i = 0; i < size; ++i)    // считаем минимум
+    for(int i = 0; i < size; ++i)    // считаем минимум
     {
-        if ( M[i] < 0 )
+        if(M[i] < 0 )
         {
             last_min = M[i];
         }
     }
-        printf ("last min = %d\n", last_min);
+        printf("last min = %d\n", last_min);
 
     for(int i = 0; i <size; ++i)    // меняем местами минимум и максимум
     {
-        if (M[i] == max)
-            updated_M [index_updated_M] = last_min;
-        else if (M[i] == last_min)
-            updated_M [index_updated_M] = max;
-        else updated_M [index_updated_M] = M[i];
+        if(M[i] == max)
+            updated_M[index_updated_M] = last_min;
+        else if(M[i] == last_min)
+            updated_M[index_updated_M] = max;
+        else updated_M[index_updated_M] = M[i];
             ++index_updated_M;
     }
 
     for(int i = 0; i < index_updated_M; ++i)    // результат
-        printf ("%d ", updated_M [i]);
+        printf("%d ", updated_M[i]);
 
-    printf ("\n");
+    printf("\n");
     return 0;
 }
