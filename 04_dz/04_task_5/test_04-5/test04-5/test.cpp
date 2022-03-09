9694db242b2e5b02529765468aa44fbc0cb4d8e8
Valeria <ler.valeu@gmail.com>
2022-03-09 22:17:55 +0300

    нужно придумать еще тесты

--- 04_dz/04_task_5/test_04-5/test04-5/test.cpp
+++ 04_dz/04_task_5/test_04-5/test04-5/test.cpp
@@ -8,8 +8,31 @@ TEST (Test_dz_4_task_5, test1)
     const char* result_str = "ww eeee ";
     char result[255] = {};
 
-    part1 (str, result);
+    part1(str, result);
 
 EXPECT_STREQ(result, result_str);
+}
 
+TEST (Test_dz_4_task_5, test2)
+
+{
+    const char* str = " ";
+    const char* result_str = " ";
+    char result[255] = {};
+
+    part1(str, result);
+
+EXPECT_STREQ(result, result_str);
+}
+
+TEST (Test_dz_4_task_5, test3)
+
+{
+    const char* str = "111 11";
+    const char* result_str = "11 ";
+    char result[255] = {};
+
+    part1(str, result);
+
+EXPECT_STREQ(result, result_str);
 }
