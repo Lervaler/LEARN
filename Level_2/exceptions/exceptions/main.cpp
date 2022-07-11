834efcde073f1ba1a82a1bac5b0e673f96f74dc5
Valeria <ler.valeu@gmail.com>
2022-07-11 15:14:22 +0300

    try catch

--- Level_2/exceptions/exceptions/main.cpp
+++ Level_2/exceptions/exceptions/main.cpp
@@ -1,19 +1,38 @@
 #include <iostream>
 
-struct MyException
+struct MyException0
+{};
+struct MyException1
+{};
+struct MyException2
 {};
 
 int func()
 {
-    throw MyException{};
+    throw MyException1{};
 }
 
 int main()
 {
-    std::set_terminate([](){
-        std::cout << "Unhandled exception" << std::endl;
-        std::abort();
-         });
-    func();
+    try
+    {
+        try
+        {
+            func();
+        } catch(MyException0& ex)
+        {
+            std::cout <<"cathed MyEx_0"<<std::endl;
+        }catch(MyException1& ex)
+        {
+            std::cout <<"cathed MyEx_1"<<std::endl;
+            throw MyException2{};
+        }
+    }
+    catch(MyException1& ex)
+    {
+        std::cout <<"cathed MyEx_1 DONE"<<std::endl;
+    }
+    std::cout <<"DONE"<<std::endl;
+
     return 0;
 }
