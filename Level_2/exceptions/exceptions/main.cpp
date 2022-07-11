7cf96b60b6d36cb5d5d6e8fdab319700044ed41a
Valeria <ler.valeu@gmail.com>
2022-07-11 15:37:32 +0300

    Для конструктора

--- Level_2/exceptions/exceptions/main.cpp
+++ Level_2/exceptions/exceptions/main.cpp
@@ -1,42 +1,26 @@
 #include <iostream>
+#include <memory>
 
-struct MyException0
-{};
-struct MyException1
-{};
-struct MyException2
-{};
+struct MyException {};
 
-int func()
+struct A
 {
-    throw MyException1{};
-}
+    std::unique_ptr<int> data = nullptr;
+
+    A()
+    {
+        std::unique_ptr<int> tmp = std::make_unique<int>();
+        throw MyException{};
+        std::swap(data, tmp);
+    }
+};
 
 int main()
 {
-    try
-    {
-        try
-        {
-            func();
-        } catch(MyException0& ex)
-        {
-            std::cout <<"cathed MyEx_0"<<std::endl;
-        }catch(MyException1& ex)
-        {
-            std::cout <<"cathed MyEx_1"<<std::endl;
-            throw MyException2{};
-        }
-    }
-    catch(MyException1& ex)
-    {
-        std::cout <<"cathed MyEx_1 DONE"<<std::endl;
-    }
-    catch(...)
-    {
-        std::cout <<"cathed any DONE"<<std::endl;
-    }
-    std::cout <<"DONE"<<std::endl;
+    try {
+        A a;
+    }  catch (...)
+    {}
 
     return 0;
 }
