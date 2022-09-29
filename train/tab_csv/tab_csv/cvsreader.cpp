8171a0bb66678b656e81447eff3351b70db04368
Valeria <ler.valeu@gmail.com>
2022-09-29 13:44:24 +0300

    готово

--- train/tab_csv/tab_csv/cvsreader.cpp
+++ train/tab_csv/tab_csv/cvsreader.cpp
@@ -1,21 +1,21 @@
 #include "cvsreader.h"
 
-#include <iostream>
-#include <fstream>
-#include <iterator>
 #include <algorithm>
+#include <iostream>
+#include <iterator>
+#include <fstream>
 #include <sstream>
 
-void Cvsreader::read_tab(const std::string& path)
+void Cvsreader::read_tab(const std::string& path)  // принимает путь к файлу
 {
     std::ifstream file(path);
 
-    std::string symbol;                    // для подсчета имени в строке
-    std::string word;                      //построчные слова
-    std::vector<std::string> new_word;    // vector of names - по длине первой строки определяется смещение
-    std::vector<std::string> all_tab;
+    std::string symbol;                  // для подсчета букв в имени колонки
+    std::string word;                    //целое имя колонки
+    std::vector<std::string> new_word;  // здесь хранятся все имена колонок + vector of names - по количеству колонок определяется смещение
+    std::vector<std::string> all_tab;   // здесь хранятся значения из таблицы и имена(номера) строк вместе
 
-    file >> word;
+    file >> word; // сюда считывается только одна первая строка целиком (имена колонок: ,A,B,Cell)
     std::string::iterator iter = word.begin();
     for(; iter != word.end(); ++iter)
     {
@@ -25,19 +25,19 @@ void Cvsreader::read_tab(const std::string& path)
         }
         if (*iter == ',')
         {
-            new_word.emplace_back(symbol);
+            new_word.emplace_back(symbol); // записываем в вектор по отдельному имени колонки
             symbol.clear();
         }
         if(iter == word.end() -1)
         {
-            new_word.emplace_back(symbol);
+            new_word.emplace_back(symbol); // записывает последнее имя в колонке
             symbol.clear();
         }
     }
 
     while(!file.eof())
     {
-        file >> word;
+        file >> word; // теперь сюда считывается все подряд пока не дойдет до конца файла
         std::string::iterator itr = word.begin();
         for(; itr != word.end(); ++itr)
         {
@@ -47,12 +47,12 @@ void Cvsreader::read_tab(const std::string& path)
             }
             if (*itr == ',')
             {
-                all_tab.emplace_back(symbol);
+                all_tab.emplace_back(symbol); // записывает в вектор каждое полученное значение
                 symbol.clear();
             }
             if(itr == word.end() -1)
             {
-                all_tab.emplace_back(symbol);
+                all_tab.emplace_back(symbol); // записывает каждое последнее значение в строке
                 symbol.clear();
             }
         }
@@ -60,7 +60,7 @@ void Cvsreader::read_tab(const std::string& path)
     file.close();
 
     // фиксим таблицу
-    while(search(all_tab) == true)
+    while(search(all_tab) == true) // search проверяет есть ли еще значения в таблице типа =A1+B1
     {
         fix_tab(new_word, all_tab);
     }
@@ -69,10 +69,9 @@ void Cvsreader::read_tab(const std::string& path)
     for(int i = 1; i < new_word.size(); ++i)
     {
         std::cout<<","<<new_word[i];
-        names_colomn.push_back(",");
+        names_colomn.push_back(",");  // записала в объект names_colomn значения...зачем? возможно, чтобы дальше можно было пользоваться, хотя по сути это не нужно
         names_colomn.push_back(new_word[i]);
     }
-
     std::cout <<std::endl;
 
     for(int j = 1; j <= all_tab.size(); ++j)
@@ -89,36 +88,33 @@ void Cvsreader::read_tab(const std::string& path)
             std::cout<<std::endl;
         }
     }
-
-    int a = 8;
-    for(const auto& elem: names_colomn)
-    std::cout << elem;
-
 }
 
 void Cvsreader::fix_tab(std::vector<std::string> new_word,  std::vector<std::string>& all_tab)
 {
+    // graph_for_fix ищет колонку со значением "=" внутри, если такой нет возращаемое значение равно all_tab.end()
     auto graph_for_fix =  std::find_if(all_tab.begin(), all_tab.end(),
-                              [](const std::string& value)
-                              {
-                                  return value[0] == '=';
-                              }
-                          );
+                                       [](const std::string& value)
+                                       {
+                                           return value[0] == '=';
+                                       }
+                                       );
 //    std::cout<< *graph_for_fix << std::endl;
 
     if(graph_for_fix != all_tab.end())
     {
         std::string formating = *graph_for_fix;        // сюда записана строка типа =A1+B1
 
-        std::string arg1;
-        std::string num_arg1;
-        std::string arg2;
-        std::string num_arg2;
-        char op = 0;
+        std::string arg1; // сюда запишем название первой колонки указывающей откуда брать значение
+        std::string num_arg1; // сюда запишем номер колонки указывающей откуда брать первое значение
+        std::string arg2;// сюда запишем название второй колонки указывающей откуда брать значение
+        std::string num_arg2;// сюда запишем номер колонки указывающей откуда брать второе значение
+        char op = 0; // сюда запишем операцию, производимую над колонками
 
         std::string::iterator itr1 = formating.begin()+1;
-        std::string::iterator itr2;
+        std::string::iterator itr2; // здесь пока пусто, но потом сиюда запишется указатель на второе значение из колонки
 
+        // в первом цикле считываем первую колонку, номер первой колонки и операцию
         for(; itr1 != formating.end(); ++itr1)
         {
             if((*itr1 >='A' && *itr1 <='Z') || (*itr1 >='a' && *itr1 <='z'))
@@ -132,39 +128,40 @@ void Cvsreader::fix_tab(std::vector<std::string> new_word,  std::vector<std::str
             if (*itr1 == '+' || *itr1 == '-' || *itr1 == '/' || *itr1 == '*')
             {
                 op = *itr1;
-                itr2 = itr1;
+                itr2 = itr1; // итератор2 теперь указывает на значение после операции
                 break;
             }
         }
-            for(; itr2 != formating.end(); ++itr2)
-           {
-                    if((*itr2 >='A' && *itr2 <='Z') || (*itr2 >='a' && *itr2 <='z'))
-                    {
-                        arg2 += *itr2;
-                    }
-                    if(*itr2 >='0' && *itr2 <='9')
-                    {
-                        num_arg2 += *itr2;
-                    }
-            }
+        // второй цикл запишет второе значение и номер колонки
+        for(; itr2 != formating.end(); ++itr2)
+        {
+                 if((*itr2 >='A' && *itr2 <='Z') || (*itr2 >='a' && *itr2 <='z'))
+                 {
+                     arg2 += *itr2;
+                 }
+                 if(*itr2 >='0' && *itr2 <='9')
+                 {
+                     num_arg2 += *itr2;
+                 }
+        }
 
     //  ищем по итераторам аргументов значения, на которые указывают элементы
 
         auto iter1 = find(new_word.begin(), new_word.end(), arg1);
-        if(iter1 == new_word.end())
+        if(iter1 == new_word.end()) // если таблица битая, то find не найдет такого имени колонки и вернет указатель на end() и тогда все ляжет
         {
             std::cout << "no colomn1 in tab!!"<< std::endl;
             throw std::exception{};
         }
         auto iter2 = find(new_word.begin(), new_word.end(), arg2);
-        if(iter2 == new_word.end())
+        if(iter2 == new_word.end()) // если таблица битая, то find не найдет такого имени колонки и вернет указатель на end() и тогда все ляжет
         {
             std::cout << "no colomn2 in tab!!"<< std::endl;
             throw std::exception{};
         }
 
-        std::string first;
-        std::string second;
+        std::string first; // сюда считается первое значение из таблицы в виде строки
+        std::string second; // сюда считается второе значение из таблицы в виде строки
 
         for(int i = 0; i < all_tab.size(); ++i * new_word.size())
         {
@@ -186,11 +183,11 @@ void Cvsreader::fix_tab(std::vector<std::string> new_word,  std::vector<std::str
 
         // подсчет операции двух индексов
 
-        int first_int = std::stoi(first);
+        int first_int = std::stoi(first); // переделываем значения в int
         int sedcond_int = std::stoi(second);
-        int result = 0;
+        int result = 0; // сюда запишем результат операции
 
-        switch(op)
+        switch(op) // здесь считается операция
         {
             case '+':
             {
@@ -212,7 +209,7 @@ void Cvsreader::fix_tab(std::vector<std::string> new_word,  std::vector<std::str
                 result = first_int / sedcond_int;
                 break;
             }
-            default:
+            default: // если не было считано нужной операции все упадет
             {
                 std::cout << "no operation for do!!"<< std::endl;
                 throw std::exception{};
@@ -233,14 +230,14 @@ void Cvsreader::fix_tab(std::vector<std::string> new_word,  std::vector<std::str
     }
 }
 
-bool Cvsreader::search(std::vector<std::string>& all_tab)
+bool Cvsreader::search(std::vector<std::string>& all_tab) // ищем есть ли в таблице выражение с '='
 {
     auto graph_for_fix =  std::find_if(all_tab.begin(), all_tab.end(),
-                              [](const std::string& value)
-                              {
-                                  return value[0] == '=';
-                              }
-                          );
+                                       [](const std::string& value)
+                                       {
+                                           return value[0] == '=';
+                                       }
+                                       );
     if(graph_for_fix == all_tab.end())
         return false;
     else return true;
--- train/tab_csv/tab_csv/main.cpp
+++ train/tab_csv/tab_csv/main.cpp
@@ -1,16 +1,21 @@
-#include <iostream>
 #include "cvsreader.h"
 
+#include <iostream>
 
-int main()
+int main(int argc, char* argv[])
 {
+    constexpr int parametrs = 2;
+
+    if(argc != parametrs)
+    {
+        return -1;
+    }
+
+    std::string cvs_filename = argv[1];
 
     Cvsreader reader;
-
-    reader.read_tab("D:/01_Projects/HomeWork/LEARN/LEARN/train/tab_csv/tab_csv/tab2.csv");
-
-
-
+    reader.read_tab(cvs_filename);
 
     return 0;
 }
+//"D:/01_Projects/HomeWork/LEARN/LEARN/train/tab_csv/tab_csv/tab2.csv"
